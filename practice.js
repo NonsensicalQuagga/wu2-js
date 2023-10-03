12d16e960a5cdca864bc39e8bc6dd785b80f91e0
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-10-03 10:36:18 +0200

    created a button that's called pog. when pressed changes text to pog^(1 + x). x = times pressed

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-export function setupPractice (element) {
+export function setupPractice(element) {
   // följande funktioner körs i ordning
   // se output i console.log()
   // du ska i varje del arbeta med att visa vad du lärt dig
@@ -11,15 +11,15 @@ export function setupPractice (element) {
   operators(element)
   controlStructures(element)
   arraysAndObjects(element)
-  /*
   domAndEvents(element)
+  /*
   domManipulation(element)*/
 }
 
 // du kan använda kontrollstrukturer och variabler etc. i 
 // andra moment än där funktionen heter så såklart
-function varsAndTypes(element){
-    
+function varsAndTypes(element) {
+
   var wow;
   let x = Math.ceil(Math.random() * 100);
   wow = document.createElement("div");
@@ -33,7 +33,7 @@ function operators(element) {
   //              :(
   // Jag får inte använda min var :(
   // if(parseInt(wow.textContent) > 50);
-  if(parseInt(element.children[0].textContent) > 50){
+  if (parseInt(element.children[0].textContent) > 50) {
     let newElement = document.createElement("h1");
     newElement.textContent = "Talet är större än 50";
     element.appendChild(newElement);
@@ -41,41 +41,54 @@ function operators(element) {
 }
 
 function controlStructures(element) {
- console.log(yeet);
-  try{
+  console.log(yeet);
+  try {
     console.log(element.children[1].textContent);
-    while(true){
-      try{
+    while (true) {
+      try {
         if (yeet === undefined) pog;
-      if(yeet < parseInt(element.children[0].textContent)){
-        element.children[1].textContent += "!";
-        yeet++
+        if (yeet < parseInt(element.children[0].textContent)) {
+          element.children[1].textContent += "!";
+          yeet++
+        }
+        else break;
       }
-      else break;}
-      catch(error){
-        var yeet = 50;}
+      catch (error) {
+        var yeet = 50;
+      }
     }
-    }
-  catch(error){
+  }
+  catch (error) {
     let bad = document.createElement("h1");
     bad.textContent = "SÄMST";
-    element.appendChild(bad)
+    element.appendChild(bad);
   }
+
+}
+
+function arraysAndObjects(element) {
+  // kod för att visa vad du lärt dig om arrays och objekt
+  console.log(['arrays', { and: 'objects' }])
+  let numbers = [1, 2, 3, 4, 5]
   
 }
 
-function arraysAndObjects (element) {
-  // kod för att visa vad du lärt dig om arrays och objekt
-  console.log(['arrays', {and: 'objects'}])
-}
-
-function domAndEvents (element) {
+function domAndEvents(element) {
   // kod för att visa vad du lärt dig om DOM och events
   // använd med html och listeners
-  console.log(element, 'lyssna efter event för att ändra element')
+  let newElement = document.createElement("button");
+  let newElementText = document.createElement("h1");
+  newElementText.textContent = "pog";
+  newElement.appendChild(newElementText);
+  let factor = 1;
+  newElement.addEventListener('click', (e) =>{
+    factor++;
+    newElement.children[0].textContent = "pog^" + factor;
+  })
+  element.appendChild(newElement);
 }
 
-function domManipulation (element) {
+function domManipulation(element) {
   // kod för att visa vad du lärt dig om DOM-manipulation
   // skapa element och lägga till och ta bort
   element.innerHTML = 'dom manipulation, ändra och lägg till element'
